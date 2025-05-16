4d157837673284b58053c9a586b8d83350746b6a
Dev Raj R <dev.raj.r1729@gmail.com>
2025-05-16 23:43:22 +0530

    change fit term calculation

--- segmentation.m
+++ segmentation.m
@@ -1,4 +1,4 @@
-clear figcount;
+clear all;
 close all;
 %Helper function for next figure count
 global figcount;
@@ -10,85 +10,54 @@ function c = nfc()
 end
 
 % Control parameters
-mu = 0.2; % Weight for penalty term
-nu = 1.5;
-lambda = 1;
-gam = 1;
+mu = 0.0002; % Weight for reg term
+nu = 1; % weight for line term
+lambda = 20; % weight for fit term
+gam = 0.2; % weight for fractional fit term
 
-delta_t = 5;
+delta_t = 1;
 sigma = 3;
 epsilon = 1.5; % smoothing For dirac delta and heaviside
 alpha = 1;
 beta = 0.8;
-no_iter = 100;
-out_every = 1; % no iteration after which plot is updated
+no_iter = 10000;
+out_every = 1000; % no iteration after which plot is updated
 
-% Continuous Dirac Delta
-% syms DiracDeltasym(x)
-% DiracDeltasym(x) = piecewise(abs(x) < epsilon,1/(2*epsilon)*(1 + cos(pi*x/epsilon)),0 );
-% figure(nfc());
-% fplot(DiracDeltasym);
-% title("Dirac Delta using symbolic");
-
-function out = DiracDelta(x,epsilon)
-    out = (abs(x) < epsilon).*((1 + cos(pi* x / epsilon))/(2*epsilon));
-end
+% function out = DiracDelta(x,epsilon)
+%     out = (abs(x) < epsilon).*((1 + cos(pi* x / epsilon))/(2*epsilon));
+% end
 % figure(nfc());
 % fplot(@(x) DiracDelta(x,epsilon));
 % title("Dirac Delta")
 
 
 % Continuous Heaviside Function
-function out = Heaviside(x,epsilon)
-    out = (abs(x) < epsilon) .* (0.5*(1 + x/epsilon + (1/pi)*sin(pi*x/epsilon))) +...
-        (x > epsilon);
+% function out = Heaviside(x,epsilon)
+%     out = (abs(x) < epsilon) .* (0.5*(1 + x/epsilon + (1/pi)*sin(pi*x/epsilon))) +...
+%         (x > epsilon);
+% end
+
+%% Heaviside function 
+function h = Heaviside(x,e)
+   h =  0.5 .*(1 + (2*atan(x/e))/pi);
 end
 
-% figure(nfc());
-% fplot(@(x) Heaviside(x,epsilon));
-% title("Heaviside");
+%% Dirac Delta function
+function d = DiracDelta(x,e)
+    d = e./(pi*(e^2 + x.^2));
+end
 
 % I = rgb2gray(imread("drlse_knee.jpg"));
-I = imread("coins.png");
+I = imread("inputs/oval_and_rectangle.png");
 I = double(I)/255; % Normalize Image
-% I2 = imread("coins.png");
-% writematrix(I2,"coins.csv");
 
-
-% coins_size = size(I2)
-% I = zeros([420,420]);
-% I(200:300,200:300) = 1;
 gauss_filter = fspecial('gaussian',6*ceil(sigma)+1,sigma);
-I = conv2(I,gauss_filter,"same");
-% writematrix(gauss_filter,"gaussian.csv");
+% I = conv2(I,gauss_filter,"same");
 
-% figure(nfc());
-% imshow(I);
-% title("Image");
-
-% Calculate 
-% [Ix,Iy] = imgradientxy(I);
-% grad_I = abs(Ix) + abs(Iy);
 [grad_I,grad_dir] = imgradient(I);
-figure(nfc());
-imshow(grad_I);
-title("Absolute Gradient");
-writematrix(grad_I,"./matrices/gradient.csv");
-
 % Calculate Order Matrix 
 P = (255*grad_I + alpha)./ (255*grad_I + beta);
-writematrix(P,"./matrices/order_matrix.csv");
 
-% Calculate Fractional Derivative
-% tic
-% I_frac = FracDerIterative(I,P);
-% toc
-% writematrix(I_frac,"iterative_frac.csv");
-% figure(nfc());
-% imshow(I_frac);
-% title("Iterative Fractional Derivative");
-
-% Calculative Fractional Derivative using convolution
 tic
 I_frac = FracDerConv(I,P);
 toc
@@ -97,49 +66,19 @@ figure(nfc());
 imshow(I_frac);
 title("Convolution Fractional Derivative");
 
-
-% tic 
-% I_frac3 = FracDerApprox(I,P,100,alpha,beta);
-% toc
-% writematrix(I_frac3,"frac_approx.csv");
-% figure(nfc());
-% imshow(I_frac3./max(I_frac3,[],"all"));
-% title("Approximate Fractional Derivative");
-
-% max_error = max(abs(I_frac3-I_frac2),[],"all")
-% max_order = max(P,[],"all")
-% min_order = min(P,[],"all")
-
 %============================================
 % Calculate edge stop function 
 I_frac_smooth = conv2(I_frac,gauss_filter,"same");
 % edgeStop = 1./(1 + I_frac_smooth);
-edgeStop = 1./(1 + 127*I_frac_smooth);
+edgeStop = 1./(1 + I_frac_smooth);
 figure(nfc());
 imshow(edgeStop);
 title("Edge Stop Function");
 
-% =========================
-% Define terms for regularization penalty
-% syms dr(x)
-% dr(x) = piecewise(x==0,1,x<1,sin(2*pi*x)/(2*pi*x),x>=1,1-1/x);
-function out = dr(x)
-    zrs = (x==0);
-    out =  zrs + ...
-            (x > 0 & x <1).*(sin(2*pi*x)./(2*pi*x + zrs)) + ...
-            (x >= 1).*(1 - 1./(x+zrs) );
-
-end
-
-% figure(nfc());
-% fplot(@(x) dr(x),[0,2]);
-% title("Double well potential d_R");
-
-
 % ========
 % Initialize Level Set
 phi =  2*ones(size(I));
-phi(140:210,200:270) = -2;
+phi(32:224,32:224) = -2;
 figure(nfc());
 imshow(I,[]);hold on;
 contour(phi, [0 0], 'r', 'LineWidth', 1);
@@ -149,65 +88,59 @@ figure(nfc());
 
 line_terms = zeros([1,no_iter]);
 area_terms = zeros([1,no_iter]);
-pen_terms = zeros([1,no_iter]);
+reg_terms = zeros([1,no_iter]);
 fit_terms = zeros([1,no_iter]);
 frac_fit_terms = zeros([1,no_iter]);
+
+K = gauss_filter;
+KI = imfilter(I,K,'replicate');
+FI = I + I_frac;
+FKI = imfilter(FI,K,"replicate");
+
 tic
 for iter = 1:no_iter
+    phi = NeumannBoundCond(phi);
     [phi_x,phi_y] = gradient(phi);
-    grad_phi = sqrt(phi_x.^2 + phi_y.^2 + 1e-10);
-
-    L = zeros(size(I)); % L(phi_{i,j}^n)
-    Delta_phi = DiracDelta(phi,epsilon);
+    grad_phi = sqrt(phi_x.^2 + phi_y.^2) + 1e-10;
+    delta_phi = DiracDelta(phi,epsilon);
     H_phi = Heaviside(phi,epsilon);
-
-    %% Penalty term calculation i.e mu*div(d_R|grad phi| . grad phi)
-    dr_coeff = dr(grad_phi);
-    pen_x = dr_coeff.*phi_x;
-    pen_y = dr_coeff.*phi_y;
-    pen_term = div(pen_x,pen_y);
-    pen_terms(iter) = mean(pen_term,"all");
-    L = L + mu .*pen_term;
-
-    %% Line term
-    coeff = edgeStop ./(grad_phi); 
-    line_x = coeff .* phi_x;
-    line_y = coeff .* phi_y;
-    line_term =  Delta_phi.*div(line_x, line_y ); 
-    line_terms(iter) = mean(line_term,"all");
-    L = L + nu* line_term ;
-
-    %% Area term 
-    % area_term = edgeStop.*DiracDelta(phi,epsilon);
-    % area_terms(iter) = mean(area_term,"all");
-    % L = L + gam*edgeStop.*DiracDelta(phi,epsilon);
-
-    %% fitting term 
-    KIH_1 = imfilter(I.* H_phi,gauss_filter,'replicate');
-    KH_1 = imfilter(H_phi,gauss_filter,'replicate') + 1e-10;
+    %% Regularization
+    dr_phi = dr(grad_phi);
+    reg_term = divergence(dr_phi.*phi_x,dr_phi.*phi_y);
+    reg_terms(iter) = mu*max(abs(reg_term),[],"all");
+    %% Line Term
+    div_gphi = divergence(edgeStop.*phi_x ./ grad_phi,...
+                        edgeStop.*phi_y./ grad_phi);
+    line_term = delta_phi.*div_gphi;
+    line_terms(iter) = nu*max(abs(line_term),[],"all");
+    %% fit term
+    KIH_1 = imfilter(I.*H_phi,K,"replicate");
+    KH_1 = imfilter(H_phi,K,"replicate");
     f1 = KIH_1 ./ KH_1;
 
-    KIH_2 = imfilter(I.*(1 - H_phi),gauss_filter,'replicate');
-    KH_2 = imfilter(1 - H_phi,gauss_filter,'replicate') + 1e-10;
+    KIH_2 = KI - KIH_1;
+    KH_2 = 1 - KH_1;
     f2 = KIH_2 ./ KH_2;
 
-    fitting_term = Delta_phi.*((I - f2).^2 - (I - f1).^2);
-    fit_terms(iter) = mean(fitting_term,"all");
-    L = L + lambda*fitting_term;
+    % R1 is zero because lambda_1 = lambda_2
+    R2 = I.*imfilter(f1 - f2,K,"replicate");
+    R3 = imfilter(f1.^2 - f2.^2,K,"replicate");
+    fit_term = -delta_phi.*( R3 - 2*R2);
+    fit_terms(iter) = lambda*max(abs(fit_term),[],"all");
+    %% Fractional Fit Terms
+    FKIH_1 = imfilter(I.*H_phi,K,"replicate");
+    FKIH_2 = FKI - FKIH_1;
+    b1 = FKIH_1 ./ KH_1;
+    b2 = FKIH_2 ./ KH_2;
 
-    %% fractional fitting term 
-    FI = I + I_frac; 
+    R2 = FI.*imfilter(b1 - b2,K,"replicate");
+    R3 = imfilter(b1.^2 - b2.^2,K,"replicate");
+    frac_fit_term = -delta_phi.*(R3 - 2*R2);
+    frac_fit_terms(iter) = gam*max(abs(frac_fit_term),[],"all");
     
-    KFIH_1 = imfilter(FI.*H_phi,gauss_filter,'replicate');
-    b1 = KFIH_1 ./ KH_1;
-
-    KFIH_2 = imfilter(FI.*H_phi,gauss_filter,'replicate');
-    b2 = KFIH_2 ./ KH_2;
-
-    frac_fit_term = Delta_phi.*((I - b2).^2 - (I - b1).^2);
-    frac_fit_term(iter) = mean(frac_fit_term,"all");
-    L = L + gam*frac_fit_term;
     %% Updation
+    L = mu*reg_term + nu*line_term + lambda*fit_term ...
+        +gam*frac_fit_term;
 
     phi = phi + delta_t*L;
     if mod(iter, out_every) == 0
@@ -219,10 +152,10 @@ for iter = 1:no_iter
 end
 toc
 % Iteration End
-figure(nfc());
-imshow((phi<0).*I);
+% figure(nfc());
+% imshow((phi<0).*I);
 % surf(phi);
-title("segmentation");
+% title("segmentation");
 writematrix(phi,"./matrices/phi.csv");
 
 figure(nfc());
@@ -231,8 +164,8 @@ imshow(I,[]);hold on;
 contour(phi, [0 0], 'r', 'LineWidth', 1);
 title("final level set");
 
-writematrix(line_terms,"./matrices/line_terms.csv");
-writematrix(area_terms,"./matrices/area_terms.csv");
+% writematrix(line_terms,"./matrices/line_terms.csv");
+% writematrix(area_terms,"./matrices/area_terms.csv");
 
 figure(nfc())
 plot(line_terms);
@@ -250,8 +183,8 @@ plot(frac_fit_terms);
 title("fractional fiting terms");
 
 figure(nfc());
-plot(pen_terms);
-title("pen terms");
+plot(reg_terms);
+title("reg terms");
 
 % Calculate Divergence
 function d = div(Fx,Fy)
@@ -322,4 +255,46 @@ function g = FracDerApprox(I,order_matrix,no_buckets,a,b)
 
         g = g + (bucket_map == bucket) .* (full_convx(1:n,1:m) + full_convy(1:n,1:m));
     end
+end
+
+function phi = NeumannBoundCond(phi)
+    % Enforce Neumann boundary conditions by copying edge pixels
+    [nrow, ncol] = size(phi);
+
+    % Corners
+    phi(1,1) = phi(3,3);
+    phi(1,ncol) = phi(3,ncol-2);
+    phi(nrow,1) = phi(nrow-2,3);
+    phi(nrow,ncol) = phi(nrow-2,ncol-2);
+
+    % Top and bottom rows
+    phi(1,2:ncol-1) = phi(3,2:ncol-1);
+    phi(nrow,2:ncol-1) = phi(nrow-2,2:ncol-1);
+
+    % Left and right columns
+    phi(2:nrow-1,1) = phi(2:nrow-1,3);
+    phi(2:nrow-1,ncol) = phi(2:nrow-1,ncol-2);
+end
+
+function fit = CalculateFitTerm(I,K,KI,H_phi,delta_phi)
+    KIH_1 = imfilter(I.*H_phi,K,"replicate");
+    KH_1 = imfilter(H_phi,K,"replicate");
+    f1 = KIH_1 ./ KH_1;
+
+    KIH_2 = KI - KIH_1;
+    KH_2 = 1 - KH_1;
+    f2 = KIH_2 ./ KH_2;
+
+    % R1 is zero as lambda_1 = lambda_2 here
+    R2 = I.*imfilter(f1-f2,K,"replicate");
+    R3 = imfilter(f1.^2 - f2.^2,K,"replicate");
+    fit = -delta_phi .*(R3 - 2*R2);
+end
+
+function out = dr(x)
+    zrs = (x==0);
+    out =  zrs + ...
+            (x > 0 & x <1).*(sin(2*pi*x)./(2*pi*x + zrs)) + ...
+            (x >= 1).*(1 - 1./(x+zrs) );
+
 end
